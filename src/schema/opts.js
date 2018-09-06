6622934ae6b8f0d22a44ef8931319f78ca3e46e5
WangJie <i@i8e.net>
2018-09-06 18:03:35 +0800

    add new options

--- src/schema/opts.js
+++ src/schema/opts.js
@@ -1,45 +1,20 @@
 const mongoose = require('mongoose')
 
 const optsSchema = new mongoose.Schema({
-  browserAction: {
-    type: String,
-  },
-  itemClickAction: {
-    type: String,
-  },
-  popupItemClickAction: {
-    type: String,
-  },
-  removeItemBtnPos: {
-    type: String,
-  },
-  addHistory: {
-    type: Boolean,
-  },
-  ignorePinned: {
-    type: Boolean,
-  },
-  pinNewList: {
-    type: Boolean,
-  },
-  pageContext: {
-    type: Boolean,
-  },
-  openTabListWhenNewTab: {
-    type: Boolean,
-  },
-  defaultNightMode: {
-    type: Boolean,
-  },
-  itemDisplay: {
-    type: String,
-  },
-  hideFavicon: {
-    type: Boolean,
-  },
-  fixedToolbar: {
-    type: Boolean,
-  },
+  browserAction: String,
+  itemClickAction: String,
+  popupItemClickAction: String,
+  removeItemBtnPos: String,
+  addHistory: Boolean,
+  ignorePinned: Boolean,
+  pinNewList: Boolean,
+  pageContext: Boolean,
+  openTabListWhenNewTab: Boolean,
+  defaultNightMode: Boolean,
+  itemDisplay: String,
+  hideFavicon: Boolean,
+  fixedToolbar: Boolean,
+  alertRemoveList: Boolean,
 })
 
 module.exports = { optsSchema }
